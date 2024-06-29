d6e054a11b414edc9b9e13cff7bd4c9d1c685d62
StanfordDC <stanforddchandra@gmail.com>
2024-06-29 17:13:15 +0800

    Added waste type model

---
+++ lib/models/wasteType.dart
@@ -0,0 +1,9 @@
+class WasteType{
+  String material;
+  String item;
+  bool recyclable;
+  String instructions;
+  String link;
+
+  WasteType(this.material, this.item, this.recyclable, this.instructions, this.link);
+}
