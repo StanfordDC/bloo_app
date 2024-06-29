a79dff8c044d79c12d12ff7a917f5d1d48284f81
StanfordDC <stanforddchandra@gmail.com>
2024-06-29 23:22:10 +0800

    Added overlay in camera preview

--- lib/screens/recycle.dart
+++ lib/screens/recycle.dart
@@ -47,34 +47,103 @@ class _RecycleState extends State<Recycle> {
             else{
               return const Center(child: CircularProgressIndicator(),);
             }
-          })
+          }),
+           Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              height: 150,
+              width: double.infinity,
+              padding: EdgeInsets.all(15),
+              color: Colors.black,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  cameraControlWidget(context),
+                ],
+              ),
+            ),
+          )
         ],
       ),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Color.fromRGBO(255, 255, 255, .7),
-        shape: const CircleBorder(),
-        onPressed: () async{
-          try{
-            await cameraValue;
+      // floatingActionButton: FloatingActionButton(
+      //   backgroundColor: Color.fromRGBO(255, 255, 255, .7),
+      //   shape: const CircleBorder(),
+      //   onPressed: () async{
+      //     try{
+      //       await cameraValue;
 
-            final image = await cameraController.takePicture();
-            if (!context.mounted) return;
+      //       final image = await cameraController.takePicture();
+      //       if (!context.mounted) return;
 
-            // If the picture was taken, display it on a new screen.
-            Navigator.pushReplacementNamed(context, '/confirmation', arguments: image.path);
-            } catch (e) {
-            // If an error occurs, log the error to the console.
-            print(e);
-            }
-        },
-        child: const Icon(
-            Icons.camera_alt,
-            size: 40,
-            color: Colors.black
+      //       // If the picture was taken, display it on a new screen.
+      //       Navigator.pushReplacementNamed(context, '/confirmation', arguments: image.path);
+      //       } catch (e) {
+      //       // If an error occurs, log the error to the console.
+      //       print(e);
+      //       }
+      //   },
+      //   child: const Icon(
+      //       Icons.camera_alt,
+      //       size: 40,
+      //       color: Colors.black
+      //   ),
+      // ),
+      // floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+
+    );
+  }
+
+  Widget cameraControlWidget(context) {
+    return Expanded(
+      child: Align(
+        alignment: Alignment.center,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          mainAxisSize: MainAxisSize.max,
+          children: <Widget>[
+            GestureDetector(
+              // child: Icon(
+              //   Icons.camera_alt,
+              //   color: Colors.black,
+              // ),
+              // backgroundColor: Color.fromRGBO(255, 255, 255, .7),
+               onTap: () async{
+                try{
+                  await cameraValue;
+
+                  final image = await cameraController.takePicture();
+                  if (!context.mounted) return;
+
+                  // If the picture was taken, display it on a new screen.
+                  Navigator.pushReplacementNamed(context, '/confirmation', arguments: image.path);
+                  } catch (e) {
+                  // If an error occurs, log the error to the console.
+                  print(e);
+                  }
+              },
+              child: Container(
+                width: 70.0,
+                height: 70.0,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  border: Border.all(color: Colors.white, width: 5.0),
+                  color: Colors.transparent,
+                ),
+                child: Center(
+                  child: Container(
+                    width: 60.0,
+                    height: 60.0,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              )
+            )
+          ],
         ),
       ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-
     );
   }
 }
