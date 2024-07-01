118cf998e06e215fc13006b8fd061b8de743ec40
StanfordDC <stanforddchandra@gmail.com>
2024-07-01 22:03:07 +0800

    Updated home page ui

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -13,27 +13,6 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color.fromRGBO(241,253, 240,1),
-      appBar: AppBar(
-        backgroundColor: Color.fromRGBO(241,253, 240,1),
-          // leading:  IconButton(
-          //   icon: const Icon(
-          //     Icons.feed_outlined,
-          //     size: 35,
-          //     color: Colors.black,
-          //   ),
-          //   onPressed:(){},
-          // ),
-          // actions: <Widget>[
-          //   IconButton(
-          //     icon: const Icon(
-          //       Icons.person,
-          //       size: 35,
-          //       color: Colors.black,
-          //     ),
-          //     onPressed:(){},
-          //   )
-          // ]
-      ),
       body: SafeArea(
       child: Center(
         child: Column(
@@ -49,12 +28,6 @@ class _HomeState extends State<Home> {
                 size: 130,
                 color: Color.fromRGBO(37,194,38,1),
               ),
-            ),
-            TextButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/recycle');
-              },
-              child: TextDisplay(Color.fromRGBO(37,194,38,1), "Start Recycling Right", 30.0),
             ),
           ],
         ),
