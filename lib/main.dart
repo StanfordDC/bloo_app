6f523506d7bbe5086e79e9945968109670379642
StanfordDC <stanforddchandra@gmail.com>
2024-07-03 00:02:33 +0800

    Added template for db connection

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,16 @@ import 'package:firebase_core/firebase_core.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await Firebase.initializeApp(
+    name: 'bloo-app',
+    options: FirebaseOptions(
+      apiKey: 'AIzaSyC_4NXebFUtXUCB3GDwlen3joeQ3ev357Q',
+      appId: '1:797587856663:android:1324c1073e3decf508ab52',
+      messagingSenderId: '797587856663',
+      projectId: 'blooapp-25ad6',
+      storageBucket: 'blooapp-25ad6.appspot.com',
+    ),
+  );
   final cameras = await availableCameras();
   final firstCamera = cameras.first;
 
